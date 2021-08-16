646d94d7fbec87453adbe1500619d52abaedc9e3
tamtran22 <hocsinhchuyentin@gmail.com>
2021-08-16 16:37:39 +0900

    210816

--- network/distribution_network.py
+++ network/distribution_network.py
@@ -10,11 +10,11 @@ from utils.utils import prod
 # Distributions
 #----------------------------------------------------------------------------
 class CategoricalDistributionNetwork(BaseNetwork):
-    def __init__(self, lr, state_shape, n_actions, device, n_hiddens=2,
+    def __init__(self, lr, input_shape, n_actions, device, n_hiddens=2,
             hidden_size=256, name='actor_categorical', 
             chkpt='./tmp') -> None:
         super().__init__(device, name, chkpt)
-        self.input_shape = state_shape
+        self.input_shape = input_shape
         self.output_shape = (n_actions,)
 
         # Flatten input tensor
@@ -39,19 +39,15 @@ class CategoricalDistributionNetwork(BaseNetwork):
 
 
 class GaussianDistributionNetwork(BaseNetwork):
-    def __init__(self, lr, state_shape, action_shape, device, n_hiddens=2,
-            hidden_size=256, reshape_action=False, name='actor_gauss',
+    def __init__(self, lr, input_shape, output_shape, device, n_hiddens=2,
+            hidden_size=256, reshape_output=False, name='actor_gauss',
             chkpt='./tmp') -> None:
         super().__init__(device, name, chkpt)
-        self.input_shape = state_shape
-        self.output_shape = action_shape
-        self.reshape_action = reshape_action
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+        self.reshape_output = reshape_output
 
         # Flaten input tensor
-        self.input = nn.Sequential(
-            nn.Linear(prod(self.state_shape), hidden_size),
-            nn.ReLU()
-        )
         self.create_input_layer(self.input_shape, hidden_size)
         self.create_hidden_layers(n_hiddens, hidden_size)
         self.sigma = nn.Sequential(
@@ -72,9 +68,9 @@ class GaussianDistributionNetwork(BaseNetwork):
         sigma = self.sigma(x)
         mu = self.mu(x)
         # reshape output as the same shape as action require.
-        if self.reshape_action:
-            mu = torch.reshape(mu, (state.shape[0],) + self.action_shape)
-            sigma = torch.reshape(sigma, (state.shape[0],) + self.action_shape)
+        if self.reshape_output:
+            mu = torch.reshape(mu, (state.shape[0],) + self.output_shape)
+            sigma = torch.reshape(sigma, (state.shape[0],) + self.output_shape)
         return mu, sigma
 
 
@@ -100,7 +96,7 @@ if __name__ == '__main__':
     #     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu"),
     #     n_hiddens=2
     # )
-    gauss = GaussianPolicyNetwork(
+    gauss = GaussianDistributionNetwork(
         lr=1e-3,
         state_shape=(2,3),
         action_shape=(2,2),
--- network/policies.py
+++ network/policies.py
@@ -21,14 +21,33 @@ class DiscreteTDActor(CategoricalDistributionNetwork):
     def act(self, state):
         prob = self.forward(state)
         dist = distributions.Categorical(prob)
+        pred_action = dist.sample()
+        logprob = dist.log_prob(pred_action)
+        # reshape output to action shape
+        # if self.reshape_output:
+        #     pred_action = torch.reshape(pred_action, (state.shape[0],) + self.action_shape)
+        #     logprob = torch.reshape(logprob, (state.shape[0],) + self.action_shape)
+        return pred_action, logprob
     def evaluate(self, state, action):
-        pass
+        action = torch.flatten(action, start_dim=1).to(self.device)
+        logprob = []
+        entropy = []
+        prob = self.forward(state)
+        for i in range(len(action)):
+            dist_i = distributions.Categorical(prob[i])
+            logprob_i = dist_i.log_prob(action[i])
+            logprob.append(logprob_i)
+            entropy.append(dist_i.entropy())
+        logprob = torch.stack(logprob)
+        entropy = torch.stack(entropy)
+        return logprob, entropy
+
 
 class ContinuousTDActor(GaussianDistributionNetwork):
     def __init__(self, lr, state_shape, action_shape, device, n_hiddens=2, 
             hidden_size=256, name='actor_TD', chkpt='./temp') -> None:
         super().__init__(lr, state_shape, action_shape, device, 
-            n_hiddens=n_hiddens, hidden_size=hidden_size, reshape_action=False,
+            n_hiddens=n_hiddens, hidden_size=hidden_size, reshape_output=True,
             name=name, chkpt=chkpt)
     def act(self, state):
         # Feeding 1 state at a time.
@@ -39,8 +58,9 @@ class ContinuousTDActor(GaussianDistributionNetwork):
         pred_action = dist.sample()
         logprob = dist.log_prob(pred_action)
         # reshape output to action shape
-        pred_action = torch.reshape(pred_action, (state.shape[0],) + self.action_shape)
-        logprob = torch.reshape(logprob, (state.shape[0],) + self.action_shape)
+        if self.reshape_output:
+            pred_action = torch.reshape(pred_action, (state.shape[0],) + self.output_shape)
+            logprob = torch.reshape(logprob, (state.shape[0],) + self.output_shape)
         return pred_action, logprob
     def evaluate(self, state, action):
         action = torch.flatten(action, start_dim=1).to(self.device)
@@ -54,13 +74,14 @@ class ContinuousTDActor(GaussianDistributionNetwork):
             entropy.append(dist_i.entropy())
         # reshape output to action shape
         logprob = torch.stack(logprob)
-        logprob = torch.reshape(logprob, (state.shape[0],) + self.action_shape)
         entropy = torch.stack(entropy)
-        entropy = torch.reshape(entropy, (state.shape[0],) + self.action_shape)
+        if self.reshape_output:
+            logprob = torch.reshape(logprob, (state.shape[0],) + self.output_shape)
+            entropy = torch.reshape(entropy, (state.shape[0],) + self.output_shape)
         return logprob, entropy
 
 if __name__=='__main__':
-    actor = TDActor(
+    actor = ContinuousTDActor(
         lr=1e-3,
         state_shape=(4,),
         action_shape=(2,),
